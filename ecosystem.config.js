813684e99078ca75f898de8a0c1ffa706a13850e
agungdhewe <agung_dhewe@yahoo.com>
2026-08-06 14:22:37 +0700

    no watch

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ export const apps = [{
 	script: './src/index.js',
 	exec_mode: 'fork', // fork, cluster
 	instances: '1', // 1, 2, 3, 4, max: gunakan semua core CPU
-	watch: ['./src', '.env'], // yang akan dipantau
+	//watch: ['./src', '.env'], // yang akan dipantau
 	ignore_watch: ['node_modules'], // Direktori yang diabaikan
 	watch_delay: 1000, // Delay sebelum restart (ms)
 }];
